11c79546eb9b5742f8c3d7f92cd5e76c3686e272
pc-rifan <pcrifan@gmail.com>
2024-07-08 13:26:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.datetime.now()
 
 form = st.form(key='Scrapping Berita')
 query = form.text_input('Masukan kata kunci')
-startDate = form.date_input('Periode awal berita', datetime.date(today.year, today.month, 1)), format="DD-MM-YYYY")
+startDate = form.date_input('Periode awal berita', datetime.date(today.year, today.month, 1), format="DD-MM-YYYY")
 endDate = form.date_input('sampai dengan', today, format="DD-MM-YYYY")
 submit = form.form_submit_button('Scrape Now!')
 
