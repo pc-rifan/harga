692810617d1968f6dd8148ab708c25088ae76937
pc-rifan <pcrifan@gmail.com>
2024-07-08 13:23:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # IMPORT LIBRARY
+import datetime
 import pandas as pd
 import streamlit as st
 import scrapBerita as sb
 
 st.subheader("SCRAPPING BERITA ONLINE")
 
+today = datetime.datetime.now()
+
 form = st.form(key='Scrapping Berita')
 query = form.text_input('Masukan kata kunci')
-startDate = form.text_input('Periode awal berita (format: DD-MM-YYYY)')
-endDate = form.text_input('Periode akhir berita (format: DD-MM-YYYY)')
+startDate = form.date_input('Periode awal berita', datetime.date(today.year, today.month, 1)), format="DD-MM-YYYY")
+endDate = form.date_input('sampai dengan', today, format="DD-MM-YYYY")
 submit = form.form_submit_button('Scrape Now!')
 
 df = pd.DataFrame()
