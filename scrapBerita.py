323c520023e5e3141ecd7a7eb523c09c3f38c3ef
pc-rifan <pcrifan@gmail.com>
2024-07-08 13:56:18 +0700

    Update scrapBerita.py

--- scrapBerita.py
+++ scrapBerita.py
@@ -43,3 +43,31 @@ def antaraNewsJatim(query, startDate, endDate):
 	df = pd.DataFrame(data)
 	
 	return(df)
+
+def detikJatim(query, startDate, endDate)
+
+	page = 1
+	startDate = startDate.replace("-", "/")
+	endDate = endDate.replace("-", "/")
+	judul = []; tanggal = []; link = []
+
+	while True:
+		url = "https://www.detik.com/search/searchall?query=" + query + "&page=" + str(page) + "&result_type=latest&siteid=119&fromdatex=" + startDate + "&todatex=" + endDate
+		webpage = requests.get(url)
+		soup = BeautifulSoup(webpage.content, "html.parser")
+		elements = soup.find_all("article", class_="list-content__item")
+		
+		for element in elements:
+			judul.append(element.find("a")["dtr-ttl"])
+			tanggal.append(element.find("span").text)
+			link.append(element.find("a")["href"])
+		
+		if len(judul) < 10 or page == maxPage:
+			break
+		
+		page = page + 1
+		
+	data = {'judul': judul, 'tanggal': tanggal, 'link': link}
+	df = pd.DataFrame(data)
+	
+	return(df)
