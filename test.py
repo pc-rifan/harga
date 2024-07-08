b104405e66a62700bfa3e73f0a7833de9aa1054c
pc-rifan <pcrifan@gmail.com>
2024-07-08 14:52:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -12,12 +12,9 @@ today = datetime.datetime.now()
 
 form = st.form(key='Scrapping Berita')
 query = form.text_input('Masukan kata kunci')
-startDate = form.date_input('Periode awal berita', datetime.date(today.year, today.month, 1), format="DD-MM-YYYY")
-endDate = form.date_input('sampai dengan', today, format="DD-MM-YYYY")
+startDate = form.date_input('Periode awal berita', datetime.date(today.year, today.month, 1), format="DD-MM-YYYY").strftime("%D-%M%-%Y")
+endDate = form.date_input('sampai dengan', today, format="DD-MM-YYYY").strftime("%D-%M%-%Y")
 submit = form.form_submit_button('Scrape Now!')
-
-startDate = datetime.strptime(startDate, "%D-%M%-%Y")
-endDate = datetime.strptime(endDate, "%D-%M%-%Y")
 
 df = pd.DataFrame()
 
